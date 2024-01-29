8bf47e73fd4cd0c5c07bd27c52e3f1d4c661f4d3
Wil Thieme <wil.thieme@protonmail.com>
2024-01-29 16:52:08 -0500

    finalize selenium upload function

--- src/forge_api.py
+++ src/forge_api.py
@@ -7,14 +7,18 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 from drop_file import drag_and_drop_file
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s : %(levelname)s : %(message)s")
 
+SPEED_INTERVAL = 3
+
 
 class ForgeURLs:
     """Contains URL strings for webpages used on the forge"""
+
     MANAGE_CRAFT: str = "https://forge.fantasygrounds.com/crafter/manage-craft"
     API_CRAFTER_ITEMS: str = "https://forge.fantasygrounds.com/api/crafter/items"
 
@@ -45,16 +49,17 @@ class ForgeItem:
     creds: ForgeCredentials
     item_id: str
 
-    def open_manage_item_page(self, driver: webdriver, urls: ForgeURLs):
+    def open_manage_item_page(self, driver: webdriver, urls: ForgeURLs) -> None:
+        """Navigate to the manage-craft/ page, login if needed, and set the per-page item count to 100"""
         driver.get(urls.MANAGE_CRAFT)
-        time.sleep(3)
+        time.sleep(SPEED_INTERVAL)
         if driver.find_element(By.ID, "login-form"):
             username_field = driver.find_element(By.NAME, "vb_login_username")
             password_field = driver.find_element(By.NAME, "vb_login_password")
             username_field.send_keys(self.creds.username)
             password_field.send_keys(self.creds.password)
             password_field.submit()
-            time.sleep(6)
+            time.sleep(SPEED_INTERVAL * 2)
         try:
             driver.find_element(By.ID, "item-list-content")
         except NoSuchElementException:
@@ -62,24 +67,23 @@ class ForgeItem:
 
         items_per_page = driver.find_element(By.NAME, "items-table_length")
         items_per_page.send_keys("100")
-        time.sleep(3)
+        time.sleep(SPEED_INTERVAL)
 
         # Click appropriate item page
         driver.find_element(By.XPATH, f"//a[@data-item-id='{self.item_id}']").click()
-        time.sleep(3)
+        time.sleep(SPEED_INTERVAL)
 
-    @staticmethod
-    def upload_item_build(driver: webdriver, new_build: Path) -> bool:
+    def upload_item_build(self, driver: webdriver, new_build: Path, urls: ForgeURLs) -> None:
         """Uploads a new build to this Forge item, returning True if successful"""
 
         # Click upload tab
         driver.find_element(By.ID, "manage-build-uploads-tab").click()
-        time.sleep(3)
+        time.sleep(SPEED_INTERVAL)
 
         # Drag file into dropzone
         dropzone = driver.find_element(By.ID, "build-upload-dropzone")
         drag_and_drop_file(dropzone, new_build)
-        time.sleep(6)
+        time.sleep(SPEED_INTERVAL)
 
         # Check if files are there
         try:
@@ -89,23 +93,25 @@ class ForgeItem:
 
         # Click dropzone submit button
         driver.find_element(By.ID, "submit-build-button").click()
-        time.sleep(18)
+        time.sleep(SPEED_INTERVAL * 5)
 
         # Check if files are gone
         try:
             driver.find_element(By.CLASS_NAME, "dz-filename")
             raise Exception("File did not upload correctly!")
         except NoSuchElementException:
-            return True
+            pass
 
-    def get_item_api_url(self) -> str:
-        """Constructs the API URL specific to this Forge item"""
-        return f"{ForgeURLs.API_CRAFTER_ITEMS}/{self.item_id}"
+        # Click back button
+        driver.get(urls.MANAGE_CRAFT)
+        time.sleep(SPEED_INTERVAL)
 
-    def set_build_channel(self, build_id: str, channel: ReleaseChannel, driver: webdriver, urls: ForgeURLs) -> bool:
-        """Sets the build channel of this Forge item to the specified value, returning True on 200 OK"""
-        driver.get(
-            urls.MANAGE_CRAFT,
-        )
-        csrf_token = driver.find_element(By.XPATH, "//meta[@name='csrf-token']").get_attribute("content")
-        headers = {"X-CSRF-Token": csrf_token}
+        # Click appropriate item page
+        driver.find_element(By.XPATH, f"//a[@data-item-id='{self.item_id}']").click()
+        time.sleep(SPEED_INTERVAL)
+
+        # Click appropriate item page
+        item_builds = driver.find_elements(By.XPATH, "//select[@class='form-control item-build-channel item-build-option']")
+        item_builds_latest = Select(item_builds[0])
+        item_builds_latest.select_by_visible_text("Live")
+        time.sleep(SPEED_INTERVAL)
--- src/main.py
+++ src/main.py
@@ -15,17 +15,16 @@ load_dotenv(Path(PurePath(__file__).parents[1], ".env"))
 def upload_make_live(driver: webdriver, item: ForgeItem, new_file: Path) -> None:
     urls = ForgeURLs()
     item.open_manage_item_page(driver, urls)
-    # item.upload_item_build(session, new_file)
-    # item.set_build_channel(item_builds[0]["id"], ReleaseChannel.LIVE, driver)  # assign newest build to live channel
+    item.upload_item_build(driver, new_file, urls)
 
 
 def main() -> None:
     pass_md5 = hashlib.md5(os.environ["FG_USER_PASS"].encode("utf-8")).hexdigest()
     creds = ForgeCredentials(os.environ["FG_USER_ID"], os.environ["FG_USER_NAME"], os.environ["FG_USER_PASS"], pass_md5)
     item = ForgeItem(creds, os.environ["FG_ITEM_ID"])
+    new_file = Path(PurePath(__file__).parents[1], os.environ["FG_UL_FILE"])
+    logging.debug("File upload path determined to be: %s", new_file)
     with webdriver.Chrome() as driver:
-        new_file = Path(PurePath(__file__).parents[1], os.environ["FG_UL_FILE"])
-        logging.debug("File upload path determined to be: %s", new_file)
         upload_make_live(driver, item, new_file)
 
 
