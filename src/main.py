9fc43a3d52a43e59b5ac4968defa538e0c084ad8
Wil Thieme <wil.thieme@protonmail.com>
2024-01-30 11:38:54 -0500

    bigger window

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ def main() -> None:
     options.add_argument('--remote-debugging-port=9222')
     options.add_argument("--headless")
     with webdriver.Chrome(service=Service(), options=options) as driver:
-        driver.set_window_size(1440, 900)
+        driver.set_window_size(1920, 1200)
         upload_make_live(driver, item, new_file)
 
 
