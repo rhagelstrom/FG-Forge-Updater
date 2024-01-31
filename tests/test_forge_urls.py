d97f0eb993129c223e5bb22900d7f8cb882d4e8b
Wil Thieme <wil.thieme@protonmail.com>
2024-01-31 13:15:22 -0500

    add url tests

---
+++ tests/test_forge_urls.py
@@ -0,0 +1,8 @@
+from src.forge_api import ForgeURLs
+
+
+def test_forge_urls() -> None:
+    """Ensure set values in ForgeURLs have not been changed"""
+    urls = ForgeURLs()
+    assert urls.MANAGE_CRAFT == "https://forge.fantasygrounds.com/crafter/manage-craft"
+    assert urls.API_CRAFTER_ITEMS == "https://forge.fantasygrounds.com/api/crafter/items"
