4c99d76f580a2ec4fb15b3395ccc97937877a01a
Wil Thieme <wil.thieme@protonmail.com>
2024-01-31 16:19:17 -0500

    ensure FG_UL_FILE environment variable exists

--- tests/main/test_construct_objects.py
+++ tests/main/test_construct_objects.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from src.forge_api import ForgeItem, ForgeURLs
@@ -6,6 +7,7 @@ from src.main import construct_objects
 
 def test_construct_objects() -> None:
     """Ensures that the object construction function provides objects of the right type in the right order"""
+    os.environ['FG_UL_FILE'] = "README.md"
     new_file, item, urls = construct_objects()
     assert isinstance(new_file, Path)
     assert isinstance(item, ForgeItem)
