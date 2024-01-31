8be1d6ce2b240f775351529a8c9a28de404a7429
Wil Thieme <wil.thieme@protonmail.com>
2024-01-30 23:06:39 -0500

    use WebDriverWait for less waiting

--- src/forge_api.py
+++ src/forge_api.py
@@ -6,13 +6,15 @@ from dataclasses import dataclass
 from pathlib import Path
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 logging.basicConfig(level=logging.WARNING, format="%(asctime)s : %(levelname)s : %(message)s")
 
-SPEED_INTERVAL = 3
+TIMEOUT = 10
 
 
 class ForgeURLs:
@@ -42,32 +44,30 @@ class ForgeItem:
     def open_manage_item_page(self, driver: webdriver, urls: ForgeURLs) -> None:
         """Navigate to the manage-craft/ page, login if needed, and set the per-page item count to 100"""
         driver.get(urls.MANAGE_CRAFT)
-        time.sleep(SPEED_INTERVAL)
-        if driver.find_element(By.ID, "login-form"):
+
+        try:
+            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.ID, "login-form")))
             username_field = driver.find_element(By.NAME, "vb_login_username")
             password_field = driver.find_element(By.NAME, "vb_login_password")
             username_field.send_keys(self.creds.username)
             password_field.send_keys(self.creds.password)
             password_field.submit()
-            time.sleep(SPEED_INTERVAL * 3)
+        except TimeoutException:
+            pass
 
         try:
-            driver.find_element(By.ID, "item-list-content")
-        except NoSuchElementException:
+            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.NAME, "items-table_length")))
+            items_per_page = Select(driver.find_element(By.NAME, "items-table_length"))
+            items_per_page.select_by_visible_text("100")
+        except TimeoutException:
             raise Exception("Could not load the Manage Craft page!")
 
-        items_per_page = driver.find_element(By.NAME, "items-table_length")
-        items_per_page.send_keys("100")
-        time.sleep(SPEED_INTERVAL)
-
         try:
+            WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.XPATH, f"//a[@data-item-id='{self.item_id}']")))
             item_link = driver.find_element(By.XPATH, f"//a[@data-item-id='{self.item_id}']")
-        except NoSuchElementException:
+            item_link.click()
+        except TimeoutException:
             raise Exception("Could not find item page, is FORGE_ITEM_ID correct?")
-
-        # Click appropriate item page
-        item_link.click()
-        time.sleep(SPEED_INTERVAL)
 
     def upload_item_build(self, driver: webdriver, new_build: Path, urls: ForgeURLs) -> None:
         """Uploads a new build to this Forge item, returning True if successful"""
@@ -75,60 +75,66 @@ class ForgeItem:
         if not new_build.is_file():
             raise Exception(f"File at {str(new_build)} is not found.")
 
-        # Click upload tab
-        driver.find_element(By.ID, "manage-build-uploads-tab").click()
-        time.sleep(SPEED_INTERVAL)
+        WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.ID, "manage-build-uploads-tab")))
+        uploads_tab = driver.find_element(By.ID, "manage-build-uploads-tab")
+        uploads_tab.click()
 
-        dz_inputs = driver.find_elements(By.XPATH, "//input[@type='file' and @class='dz-hidden-input']")
+        WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.CLASS_NAME, "dz-hidden-input")))
+        time.sleep(0.5)
+        dz_inputs = driver.find_elements(By.CLASS_NAME, "dz-hidden-input")
         dz_inputs[1].send_keys(str(new_build))
-        time.sleep(SPEED_INTERVAL)
 
-        # Check if files are there
         try:
-            driver.find_element(By.CLASS_NAME, "dz-upload")
-        except NoSuchElementException:
+            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.CLASS_NAME, "dz-upload")))
+        except TimeoutException:
             raise Exception("File drag and drop didn't work!")
 
-        # Click dropzone submit button
-        driver.find_element(By.ID, "submit-build-button").click()
-        time.sleep(SPEED_INTERVAL * 2)
+        WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.ID, "submit-build-button")))
+        submit_button = driver.find_element(By.ID, "submit-build-button")
+        submit_button.click()
 
         try:
-            # raise an exception based on the toast message, if displayed
-            driver.find_element(By.XPATH, "//*[@class='toast toast-error']")
-            toast_message = driver.find_element(By.CLASS_NAME, "toast-message")
-            raise Exception(toast_message.text)
-        except NoSuchElementException:
+            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, "//*[@class='toast toast-error']")))
+            toast_error_box = driver.find_element(By.XPATH, "//*[@class='toast toast-error']")
+            toast_message = toast_error_box.find_element(By.CLASS_NAME, "toast-message").text
+            raise Exception(toast_message)
+        except TimeoutException:
             pass
 
         try:
-            # raise an exception if dropzone shows an error message
-            dropzone_error_div = driver.find_element(By.CLASS_NAME, "dz-error-message")
-            if dropzone_error_div.value_of_css_property("display") == "block":
-                dropzone_error = dropzone_error_div.find_element(By.TAG_NAME, "span")
-                raise Exception(dropzone_error.get_attribute("innerHTML"))
-        except NoSuchElementException:
+            WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.CLASS_NAME, "dz-error-message")))
+            dropzone_error_box = driver.find_element(By.CLASS_NAME, "dz-error-message")
+            dropzone_error_box_visible = bool(dropzone_error_box.value_of_css_property("display") == "block")
+            if dropzone_error_box_visible:
+                dropzone_error_message = dropzone_error_box.find_element(By.TAG_NAME, "span").get_attribute("innerHTML")
+                raise Exception(dropzone_error_message)
+        except TimeoutException:
             pass
 
         try:
-            # raise an exception if the file is still uploading after sleeping
-            upload_progress_bar_width_filled = driver.find_element(By.CLASS_NAME, "dz-upload").value_of_css_property("width").replace("px", "")
+            upload_progress_bar = driver.find_element(By.CLASS_NAME, "dz-upload")
+            upload_progress_bar_width_filled = upload_progress_bar.value_of_css_property("width").replace("px", "")
             upload_progress_bar_width = driver.find_element(By.CLASS_NAME, "dz-progress").value_of_css_property("width").replace("px", "")
             upload_progress = float(upload_progress_bar_width_filled) / float(upload_progress_bar_width)
             raise Exception("File upload timed out at {:.0f}%".format(upload_progress))
         except NoSuchElementException:
             pass
 
-        # Click back button
         driver.get(urls.MANAGE_CRAFT)
-        time.sleep(SPEED_INTERVAL)
 
-        # Click appropriate item page
-        driver.find_element(By.XPATH, f"//a[@data-item-id='{self.item_id}']").click()
-        time.sleep(SPEED_INTERVAL)
+        try:
+            WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.XPATH, f"//a[@data-item-id='{self.item_id}']")))
+            item_link = driver.find_element(By.XPATH, f"//a[@data-item-id='{self.item_id}']")
+            item_link.click()
+        except TimeoutException:
+            raise Exception("Could not find item page, is FORGE_ITEM_ID correct?")
 
-        # Click appropriate item page
-        item_builds = driver.find_elements(By.XPATH, "//select[@class='form-control item-build-channel item-build-option']")
-        item_builds_latest = Select(item_builds[0])
-        item_builds_latest.select_by_visible_text("Live")
-        time.sleep(SPEED_INTERVAL)
+        try:
+            WebDriverWait(driver, TIMEOUT).until(
+                EC.presence_of_element_located((By.XPATH, "//select[@class='form-control item-build-channel item-build-option']"))
+            )
+            item_builds = driver.find_elements(By.XPATH, "//select[@class='form-control item-build-channel item-build-option']")
+            item_builds_latest = Select(item_builds[0])
+            item_builds_latest.select_by_visible_text("Live")
+        except TimeoutException:
+            raise Exception("Could not find item page, is FORGE_ITEM_ID correct?")
