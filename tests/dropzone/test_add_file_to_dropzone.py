7e80dac0eeae0db98c93527305eb18c71bc34da1
Wil Thieme <wil.thieme@protonmail.com>
2024-02-01 12:46:59 -0500

    improved dropzone testing

--- tests/dropzone/test_add_file_to_dropzone.py
+++ tests/dropzone/test_add_file_to_dropzone.py
@@ -16,68 +16,54 @@ TEST_ELEMENTS = [
 ]
 
 
-def find_element(by: str, value: str, elements: list[tuple[str, str]]) -> MagicMock:
-    """Construct the WebElement mock object if matching any definition in elements"""
-    if (by, value) in elements:
-        element = MagicMock(spec=WebElement)
-        element.click.return_value = None
-        element.is_enabled.return_value = True
-        element.is_displayed.return_value = True
-        element.send_keys.return_value = None
-        element.find_elements.return_value = [element, element]
-        return element
+def mock_element() -> MagicMock:
+    """Construct a mock WebElement"""
+    element = MagicMock(spec=WebElement)
+    element.click.return_value = None
+    element.is_enabled.return_value = True
+    element.is_displayed.return_value = True
+    element.send_keys.return_value = None
+    element.find_elements.side_effect = find_elements
+    return element
 
 
-def find_element_all(by: str, value: str) -> MagicMock:
-    """Construct the WebElement mock object if matching any definition in TEST_ELEMENTS"""
-    return find_element(by, value, TEST_ELEMENTS)
-
-
-def find_element_unsuccessful(by: str, value: str) -> MagicMock:
-    """Construct the WebElement mock object, but only if matching the first two definitions in TEST_ELEMENTS"""
-    return find_element(by, value, [TEST_ELEMENTS[0], TEST_ELEMENTS[1]])
+def find_element(by: str, value: str) -> MagicMock:
+    """Return a mock_element if the by, value pair isn't found in TEST_ELEMENTS"""
+    if (by, value) in TEST_ELEMENTS:
+        return mock_element()
 
 
 def find_elements(by, value) -> list[MagicMock]:
-    """Construct a list of two of the requested mock element"""
-    return [find_element_all(by, value), find_element_all(by, value)]
+    """Return two mock_elements if the by, value pair isn't found in TEST_ELEMENTS"""
+    element = find_element(by, value)
+    return [element, element]
 
 
 def test_add_file_to_dropzone() -> None:
-    """Ensure that adding files to dropzones searches for the right elements"""
+    """Ensure that element location calls are made correctly"""
 
     mock_driver = MagicMock(spec=webdriver.Chrome)
-    mock_driver.find_element.side_effect = find_element_all
+    mock_driver.find_element.side_effect = find_element
     mock_driver.find_elements.side_effect = find_elements
 
-    add_file_to_dropzone(mock_driver, 2, Path(__file__))
+    add_file_to_dropzone(mock_driver, 1, Path(__file__))
 
-    expected_find_element = [
-        call(By.XPATH, "//a[@id='manage-build-uploads-tab']"),
-        call(By.CLASS_NAME, "dz-hidden-input"),
-        call(By.CLASS_NAME, "dz-upload"),
-    ]
+    expected_find_element = [call(by, value) for (by, value) in TEST_ELEMENTS]
     assert mock_driver.find_element.mock_calls == expected_find_element
     mock_driver.find_elements.assert_called_once_with(By.CLASS_NAME, "dz-hidden-input")
 
 
 def test_add_file_to_dropzone_timeout() -> None:
-    """Ensure that exception is thrown if file is not found in dropzone after adding"""
+    """Ensure that timeout is raised if progress bar is not found after attempt to add file"""
+
+    def find_element_unsuccessful(by: str, value: str) -> MagicMock:
+        """Construct the WebElement mock object, but only if matching the first two definitions in TEST_ELEMENTS"""
+        if (by, value) in [TEST_ELEMENTS[0], TEST_ELEMENTS[1]]:
+            return mock_element()
 
     mock_driver = MagicMock(spec=webdriver.Chrome)
     mock_driver.find_element.side_effect = find_element_unsuccessful
     mock_driver.find_elements.side_effect = find_elements
 
     with pytest.raises(TimeoutException):
-        add_file_to_dropzone(mock_driver, 2, Path(__file__))
-
-    expected_find_element = [
-        call(By.XPATH, "//a[@id='manage-build-uploads-tab']"),
-        call(By.CLASS_NAME, "dz-hidden-input"),
-        call(By.CLASS_NAME, "dz-upload"),
-        call(By.CLASS_NAME, "dz-upload"),
-        call(By.CLASS_NAME, "dz-upload"),
-        call(By.CLASS_NAME, "dz-upload"),
-    ]
-    assert mock_driver.find_element.mock_calls == expected_find_element
-    mock_driver.find_elements.assert_called_once_with(By.CLASS_NAME, "dz-hidden-input")
+        add_file_to_dropzone(mock_driver, 1, Path(__file__))
--- tests/dropzone/test_dropzone_error_handling.py
+++ tests/dropzone/test_dropzone_error_handling.py
@@ -1,59 +1,79 @@
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 
 import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 from src.dropzone import DropzoneErrorHandling, DropzoneException, LongUploadException, ToastErrorException
 
 
+def mock_element() -> MagicMock:
+    """Construct a mock WebElement"""
+    element = MagicMock(spec=WebElement)
+    element.find_elements.return_value = [mock_element, mock_element]
+    return element
+
+
 def test_check_report_toast_error() -> None:
-    def find_element(by, value) -> Mock:
+    """Ensure that toast errors raise an exception containing the error text"""
+    error_text = "Never send a boy to do a woman's job."
+
+    def find_element(by, value) -> MagicMock:
+        """Returns a mock WebElement for a toast error where all child elements have a return value containing the error text"""
         if by == By.XPATH and value == "//*[@class='toast toast-error']":
-            element = Mock()
-            element.find_element.return_value.text = "Never send a boy to do a woman's job."
+            element = mock_element()
+            element.find_element.return_value.text = error_text
             return element
 
-    mock_driver = Mock()
-    mock_driver.find_element = find_element
+    mock_driver = MagicMock(spec=webdriver.Chrome)
+    mock_driver.find_element.side_effect = find_element
 
-    with pytest.raises(ToastErrorException):
+    with pytest.raises(ToastErrorException, match=error_text):
         error_handling = DropzoneErrorHandling(mock_driver)
         error_handling.check_report_toast_error()
 
 
 def test_check_report_dropzone_upload_error() -> None:
-    def find_element(by, value) -> Mock:
+    error_text = "We have no names, man. No names. We are nameless!"
+
+    def find_element(by, value) -> MagicMock:
+        """Returns a mock WebElement for a dropzone tooltip error or error message based on the by, value keys"""
         if by == By.CLASS_NAME and value == "dz-error-message":
-            element = Mock()
+            element = mock_element()
             element.value_of_css_property.return_value = "block"
+            element.find_element.side_effect = find_element
             return element
         if by == By.TAG_NAME and value == "span":
-            element = Mock()
-            element.get_attribute.return_value = "We have no names, man. No names. We are nameless!"
+            element = mock_element()
+            element.get_attribute.return_value = error_text
             return element
 
-    mock_driver = Mock()
-    mock_driver.find_element = find_element
+    mock_driver = MagicMock(spec=webdriver.Chrome)
+    mock_driver.find_element.side_effect = find_element
 
-    with pytest.raises(DropzoneException):
+    with pytest.raises(DropzoneException, match=error_text):
         error_handling = DropzoneErrorHandling(mock_driver)
         error_handling.check_report_dropzone_upload_error()
 
 
 def test_check_report_upload_percentage() -> None:
-    def find_element(by, value) -> Mock:
+    error_text = r"File upload timed out at \d+%"
+
+    def find_element(by, value) -> MagicMock:
+        """Returns a mock WebElement with all css properties returning sizes in pixels depending on the by, value pairs"""
         if by == By.CLASS_NAME and value == "dz-upload":
-            element = Mock()
+            element = mock_element()
             element.value_of_css_property.return_value = "55px"
             return element
         if by == By.CLASS_NAME and value == "dz-progress":
-            element = Mock()
+            element = mock_element()
             element.value_of_css_property.return_value = "80px"
             return element
 
-    mock_driver = Mock()
-    mock_driver.find_element = find_element
+    mock_driver = MagicMock(spec=webdriver.Chrome)
+    mock_driver.find_element.side_effect = find_element
 
-    with pytest.raises(LongUploadException):
+    with pytest.raises(LongUploadException, match=error_text):
         error_handling = DropzoneErrorHandling(mock_driver)
         error_handling.check_report_upload_percentage()
